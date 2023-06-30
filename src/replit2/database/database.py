85808884321d39d24df3202b7d5555f575745a63
py660 <93660666+py660@users.noreply.github.com>
2023-06-30 17:09:08 +0800

    log

--- src/replit2/database/database.py
+++ src/replit2/database/database.py
@@ -656,6 +656,7 @@ class Database(abc.MutableMapping):
         for i in keys:
             ret[i] = self.get_raw(i)
   
+        print("ret:", ret)
         with open(self.backup_loc, 'w') as fout:
             json.dump(ret, fout)
 
